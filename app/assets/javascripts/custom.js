66800a7df307b946db3d177181fd36d7b745f83d
NgoVanTien <ngovantien93@gmail.com>
2016-11-02 20:51:38 +0700

    Static-Pages

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,26 @@
+document.addEventListener('turbolinks:load', function() {
+  jQuery('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 5
+      }
+    }
+  });
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 400) {
+      $('.navigation1').fadeIn();
+    }
+    else {
+      $('.navigation1').fadeOut();
+    }
+  });
+});
