bbde76c02b32298156d7db508dba7625bd7f41ee
HuynhDung <huynhdungbk11@gmail.com>
2016-11-14 10:11:26 +0700

    Admin handle suggest request part 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
     root "static_pages#home"
     resources :categories
     resources :products
+    resources :suggestions
   end
   root "static_pages#home"
   devise_for :users, controllers: {omniauth_callbacks: "callbacks",
     registrations: "users/registrations"}
   resources :products, only: [:index, :show]
 end
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,25 @@
-User.create! name: "Admin", email: "admin@gmail.com", password: "123456789", role: 0
+User.create! name: "admin", email: "admin@gmail.com", password: "123456789",
+password_confirmation: "123456789", role: 0
+User.create! name: "huynhdung", email: "huynhdung@gmail.com", password: "123456789",
+password_confirmation: "123456789", role: 1
+User.create! name: "kotaro ", email: "kota@gmail.com", password: "123456789",
+password_confirmation: "123456789", role: 1
+user = User.find(id = 2)
+15.times do
+  name  = Faker::Name.name
+  description = Faker::Lorem.sentence(2)
+  message = Faker::Lorem.sentence(3)
+  price = Faker::Number.number(6)
+  Suggestion.create! name: name, description: description,
+  message: message, price: price, user_id: user.id
+end
+user = User.find(id = 3)
+
 5.times do
   name  = Faker::Name.name
-  description = Faker::Lorem.sentence(5)
-  Category.create! name: name, description: description
-end
-
-categories = Category.take(5)
-categories.each do |category|
-  15.times do
-    name = Faker::Lorem.sentence
-    description = Faker::Lorem.sentence
-    price = Faker::Number.number(6)
-    rating = rand(1..5)
-    Product.create! name: name, description: description, price: price,
-      category_id: category.id, rating: rating
-  end
+  description = Faker::Lorem.sentence(2)
+  message = Faker::Lorem.sentence(3)
+  price = Faker::Number.number(6)
+  Suggestion.create! name: name, description: description,
+  message: message, price: price, user_id: user.id
 end
